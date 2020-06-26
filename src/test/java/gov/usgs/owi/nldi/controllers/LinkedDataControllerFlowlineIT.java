70dd44b920c288b79da2154f75be5661089ea338
Kehl <kkehl@contractor.usgs.gov>
2020-06-26 10:47:56 -0700

    add unit tests to change distance range and use of null distance

--- src/test/java/gov/usgs/owi/nldi/controllers/LinkedDataControllerFlowlineIT.java
+++ src/test/java/gov/usgs/owi/nldi/controllers/LinkedDataControllerFlowlineIT.java
@@ -66,6 +66,51 @@ public class LinkedDataControllerFlowlineIT extends BaseIT {
 				false);
 	}
 
+
+	@Test
+	@DatabaseSetup("classpath:/testData/featureHuc12pp.xml")
+	public void getHuc12ppDM10000Test_ExceedsMaxDistance() throws Exception {
+		assertEntity(restTemplate,
+				"/linked-data/huc12pp/070900020601/navigate/DM?distance=10000",
+				HttpStatus.BAD_REQUEST.value(),
+				null,
+				null,
+				null,
+				null,
+				true,
+				false);
+	}
+
+	@Test
+	@DatabaseSetup("classpath:/testData/featureHuc12pp.xml")
+	public void getHuc12ppDM0Test_BelowDistance() throws Exception {
+		assertEntity(restTemplate,
+				"/linked-data/huc12pp/070900020601/navigate/DM?distance=-1",
+				HttpStatus.BAD_REQUEST.value(),
+				null,
+				null,
+				null,
+				null,
+				true,
+				false);
+	}
+
+
+	@Test
+	@DatabaseSetup("classpath:/testData/featureHuc12pp.xml")
+	public void getHuc12ppDMTest_EmptyDistance() throws Exception {
+		assertEntity(restTemplate,
+				"/linked-data/huc12pp/070900020601/navigate/DM?distance=",
+				HttpStatus.OK.value(),
+				FlowLineTransformer.FLOW_LINES_COUNT_HEADER,
+				"51",
+				BaseController.MIME_TYPE_GEOJSON,
+				null,
+				true,
+				false);
+
+	}
+
 	@Test
 	public void badInputTest() throws Exception {
 		assertEntity(restTemplate,
