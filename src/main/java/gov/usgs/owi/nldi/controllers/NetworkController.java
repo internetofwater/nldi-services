0c08bc8471ad7d7b6837e138e40734987691e12e
Kehl <kkehl@contractor.usgs.gov>
2020-07-02 12:41:05 -0700

    fix conflict in NetworkController

--- src/main/java/gov/usgs/owi/nldi/controllers/NetworkController.java
+++ src/main/java/gov/usgs/owi/nldi/controllers/NetworkController.java
@@ -39,15 +39,9 @@ public class NetworkController extends BaseController {
 			@PathVariable(Parameters.COMID) @Range(min=1, max=Integer.MAX_VALUE) String comid,
 			@PathVariable(Parameters.NAVIGATION_MODE) @Pattern(regexp=REGEX_NAVIGATION_MODE) String navigationMode,
 			@RequestParam(value=Parameters.STOP_COMID, required=false) @Range(min=1, max=Integer.MAX_VALUE) String stopComid,
-<<<<<<< HEAD
 			@Parameter(description=Parameters.DISTANCE_DESCRIPTION)
-								 @RequestParam(value=Parameters.DISTANCE, required=false)
-								 @Range(min=1, max=9999, message="distance must be between 1 and 9999 kilometers") String distance,
-=======
-			@ApiParam(value=Parameters.DISTANCE_DESCRIPTION)
 				@RequestParam(value=Parameters.DISTANCE, required=false, defaultValue=Parameters.MAX_DISTANCE)
 			@Pattern(message=Parameters.DISTANCE_VALIDATION_MESSAGE, regexp=Parameters.DISTANCE_VALIDATION_REGEX) String distance,
->>>>>>> a0fc0c76b2dffd5d747a23b22d79f4d602b9cf28
 			@RequestParam(value=Parameters.LEGACY, required=false) String legacy) throws Exception {
 		BigInteger logId = logService.logRequest(request);
 		try {
@@ -65,15 +59,9 @@ public class NetworkController extends BaseController {
 			@PathVariable(Parameters.NAVIGATION_MODE) @Pattern(regexp=REGEX_NAVIGATION_MODE) String navigationMode,
 			@PathVariable(value=DATA_SOURCE) String dataSource,
 			@RequestParam(value=Parameters.STOP_COMID, required=false) @Range(min=1, max=Integer.MAX_VALUE) String stopComid,
-<<<<<<< HEAD
 			@Parameter(description=Parameters.DISTANCE_DESCRIPTION)
-								@RequestParam(value=Parameters.DISTANCE, required=false)
-								@Range(min=1, max=9999, message="distance must be between 1 and 9999 kilometers") String distance,
-=======
-			@ApiParam(value=Parameters.DISTANCE_DESCRIPTION)
 				@RequestParam(value=Parameters.DISTANCE, required=false, defaultValue=Parameters.MAX_DISTANCE)
 			@Pattern(message=Parameters.DISTANCE_VALIDATION_MESSAGE, regexp=Parameters.DISTANCE_VALIDATION_REGEX) String distance,
->>>>>>> a0fc0c76b2dffd5d747a23b22d79f4d602b9cf28
 			@RequestParam(value=Parameters.LEGACY, required=false) String legacy) throws Exception {
 		BigInteger logId = logService.logRequest(request);
 		try {
