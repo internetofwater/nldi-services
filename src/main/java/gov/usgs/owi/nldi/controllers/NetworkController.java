8ef52ca074fe21c8d80edb930810372dc511c75d
Kehl <kkehl@contractor.usgs.gov>
2020-07-29 15:42:03 -0700

    codacy fixes

--- src/main/java/gov/usgs/owi/nldi/controllers/NetworkController.java
+++ src/main/java/gov/usgs/owi/nldi/controllers/NetworkController.java
@@ -1,6 +1,5 @@
 package gov.usgs.owi.nldi.controllers;
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +21,6 @@ import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
